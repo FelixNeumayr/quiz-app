318599973900e48d2510d4992ffdbce463d663b0
Felix <felixneumayr@gmx.de>
2024-04-15 10:17:39 +0200

    added bookmarkToggle

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const bookmarkButton = document.querySelector('[data-js="questionBox1"]');
-const answerButton = document.querySelector('[js-data"answerButton"]');
+const answerButton = document.querySelector('[data-js="answerButton"]');
+
+let bookmarkActive = false;
 
 bookmarkButton.addEventListener('click', () => {
 
+    
+
+    if (bookmarkActive === false) {
 bookmarkButton.classList.toggle('active');
-alert("Added to Bookmarks!");
+event.preventDefault();
+bookmarkActive = true;
+
+
+    } else if (bookmarkActive === true){
+        bookmarkButton.classList.toggle('active');
+        
+    }
 
 })
 
 
-answerButton.addEventListener('click' () => {
+answerButton.addEventListener('click', () => {
+    event.preventDefault();
     answerButton.classList.toggle("active");
 })
 
 
-//beides geht nur während des maus klicks
